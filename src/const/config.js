b757e3be8c5c60b498aef5a3133082a36b9c7cd1
apple <apple@appledeMacBook-Air.local>
2023-05-31 14:26:49 +0800

    style

--- src/const/config.js
+++ src/const/config.js
@@ -3,7 +3,7 @@ import abi from './abi.json'
 const config = {
     erc20_abi: abi,
     // con_addr: '0x453D7f27f3c5cDBb201bCa91818409023e1ab5Aa'
-    con_addr: '0x5E822d2c5b16F1a4Be09839a397E636DF1136Fc8',
+    con_addr: '0x365CB7Fea69efb321d1aCF6f03D3F10dAD71395A',
     chainId: '0x11623'
 }
 
