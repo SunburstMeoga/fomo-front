0eaccbd997a63cef308f4509d777846f99283856
apple <apple@192.168.10.109>
2023-06-12 14:39:55 +0800

    con_addr

--- src/const/config.js
+++ src/const/config.js
@@ -4,7 +4,7 @@ const config = {
     erc20_abi: abi,
     // rpc: 'http://192.168.3.9:8547',
     rpc: 'https://rpc-testnet.hashahead.org',
-    con_addr: '0xd3Ae2548600A8BA88aadFB83114cF8695ED49D3d',
+    con_addr: '0x5E822d2c5b16F1a4Be09839a397E636DF1136Fc8',
     chainId: '0x11623',
     chainName: 'HAH'
 }
