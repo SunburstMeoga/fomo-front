5a130a78e5dc9db91ce445cd9cfb9520ba86bace
apple <apple@appledeMacBook-Air.local>
2023-06-16 11:38:23 +0800

    con address

--- src/const/config.js
+++ src/const/config.js
@@ -4,7 +4,7 @@ const config = {
     erc20_abi: abi,
     // rpc: 'http://192.168.3.9:8547',
     rpc: 'https://rpc-testnet.hashahead.org',
-    con_addr: '0x5E822d2c5b16F1a4Be09839a397E636DF1136Fc8',
+    con_addr: '0x25D3Ff07cF8e1fF49D061e7922255C1388e9a576',
     chainId: '0x11623',
     chainName: 'HAH'
 }
