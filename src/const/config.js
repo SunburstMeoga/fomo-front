53e74ebb89046afeb94b53b7f4c82d31d44d0ef7
SunburstMeoga <1025113986@qq.com>
2025-01-21 00:44:13 +0800

    feat

--- src/const/config.js
+++ src/const/config.js
@@ -6,7 +6,7 @@ const config = {
     rpc: 'https://rpc-testnet.hashahead.org/',
     con_addr: '0xAEBe6507AF858D1006385Ebb908d5D588417C52c',
     chainId: '0x11624',
-    chainName: 'HAH'
+    chainName: 'HAH TestNet 71204 '
 }
 
 export { config }
