7122c6c52f1a87d853460ff055da0623472cbdd6
SunburstMeoga <1025113986@qq.com>
2025-01-21 00:44:00 +0800

    d

--- src/const/config.js
+++ src/const/config.js
@@ -3,10 +3,10 @@ import abi from './abi.json'
 const config = {
     erc20_abi: abi,
     // rpc: 'http://192.168.3.9:8547',
-    rpc: 'https://rpc.pgchain.org',
-    con_addr: '0x0Bd2B7338e3b3c5D0eaD07156E4895499f0A4749',
-    chainId: '0x19c2e0',
-    chainName: 'PGC'
+    rpc: 'https://rpc-testnet.hashahead.org/',
+    con_addr: '0xAEBe6507AF858D1006385Ebb908d5D588417C52c',
+    chainId: '0x11624',
+    chainName: 'HAH'
 }
 
 export { config }
